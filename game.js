720a494c886fc6207b44179b8d303ce94f60e58f
Matt Caswell <matt.caswell@hotmail.com>
2021-06-19 12:33:58 +0100

    set up of js

---
+++ game.js
@@ -0,0 +1,7 @@
+var buttonColours = ["red", "blue", "green", "yellow"]
+var randomChosenColour = buttonColours[nextSequence()]
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber;
+}
