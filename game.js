de7bf23331202c80051be43ab2a5ff3b54a99f77
Matt Caswell <matt.caswell@hotmail.com>
2021-06-20 12:44:13 +0100

    updated JS

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern =[];
+
+//function to randomly select a colour, make it flash and play audio clip
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -9,6 +12,26 @@ function nextSequence() {
     .fadeIn(100)
     .fadeOut(100)
     .fadeIn(100);
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour)
+  animatePress(randomChosenColour);
+}
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id")
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour)
+  animatePress(userChosenColour)
+}
+);
+
+function playSound(name){
+  var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
+}
+
+function animatePress (currentColour) {
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#"+currentColour).removeClass("pressed");
+  }, 100);
 }
