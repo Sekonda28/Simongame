ed02258b97a11b8946384c580c0133cd8a6a704f
Matt Caswell <matt.caswell@hotmail.com>
2021-06-20 07:44:48 +0100

    updated jQuery function

--- game.js
+++ game.js
@@ -1,38 +1,14 @@
-var gamePattern = [];
 var buttonColours = ["red", "blue", "green", "yellow"];
-var randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
+var gamePattern = [];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
-}
-
-$("#" + randomChosenColour)
-  .fadeTo("fast", 0)
-  .fadeTo("fast", 1)
-  .playSound();
-
-function playSound(randomChosenColour) {
-  switch (randomChosenColour) {
-    case "red":
-      var red = new Audio("sounds/red.mp3");
-      red.play();
-      break;
-
-    case "blue":
-      var blue = new Audio("sounds/blue.mp3");
-      blue.play();
-      break;
-
-    case "green":
-      var green = new Audio("sounds/green.mp3");
-      green.play();
-      break;
-
-    case "yellow":
-      var yellow = new Audio("sounds/yellow.mp3");
-      yellow.play();
-      break;
-  }
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
